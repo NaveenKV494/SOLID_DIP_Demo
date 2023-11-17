412415bc0e479c47b0bb2acf1fc998889b89c77b
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-17 15:52:54 +0530

    Changed new with factory in Main class

--- SOLID_DIP_Demo/Program.cs
+++ SOLID_DIP_Demo/Program.cs
@@ -10,19 +10,17 @@ namespace SOLID_DIP_Demo
     {
         static void Main(string[] args)
         {
-            IPerson owner = new Person
-            {
-                FirstName = "naveen",
-                LastName = "kumar",
-                EmailAddress = "naveen@gmail.com",
-                PhoneNumber = "7659842310"
-            };
+            IPerson owner = Factory.CreatePerson();
+            owner.FirstName = "naveen";
+            owner.LastName = "kumar";
+            owner.EmailAddress = "naveen@gmail.com";
+            owner.PhoneNumber = "7659842310";
+            
 
-            Chore chore = new Chore
-            {
-                ChoreName = "Take out the trash",
-                Owner = owner
-            };
+            IChore chore = Factory.CreateChore();
+            chore.ChoreName = "Take out the trash";
+            chore.Owner = owner;
+            
 
             chore.PerformedWork(3);
             chore.PerformedWork(1.5);
